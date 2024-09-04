2a7be51c82983f668eba546ec0deda46896d2067
Nestor.Adonay.Rodriguez.Alberto@gmail.com <nestor.adonay.rodriguez.alberto@gmail.com>
2024-09-03 22:19:59 -0600

    Creacion De La Clase Producto

---
+++ NARA20240903/Producto.cs
@@ -0,0 +1,11 @@
+﻿namespace NARA20240903
+{
+    public class Producto
+    {
+        // Atributos:
+        private int IdProducto { get; set; }
+        private string Nombre { get; set; }
+        private double Precio { get; set; }
+        private string Marca { get; set; }
+    }
+}
